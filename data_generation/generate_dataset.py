7fd9a840a54cd4f8021e18c1f9019a5f1002d83b
kabir <reachkabirjain@gmail.com>
2023-09-19 22:02:02 +0800

    Modifications to allow it to run on my system

--- data_generation/generate_dataset.py
+++ data_generation/generate_dataset.py
@@ -3,6 +3,7 @@ import sys
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 sys.path.append('/mnt/c/users/jedli/onedrive - nus high school/documents/computing studies/navier_stonks')
+sys.path.append(r'C:\\Users\\admin\\navier\\Navier-Stokes-AI')
 
 import tensorflow as tf
 
@@ -10,7 +11,7 @@ from utils import BoundaryCondition
 from data_generation import burgers_equation, fisher_kpp_equation, zpk_equation
 
 for equation in ["burgers", "fisher_kpp", "zpk"]:
-    for bc_name in ["periodic", "dirichlet", "neumann"]:
+    for bc_name in ["periodic", "dirichlet"]:
         print(f"Solving {equation} with boundary condition {bc_name}...")
 
         if equation == "burgers": equation_generator = burgers_equation
